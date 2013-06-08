61eac9ee30ab7b0d66d12b970a3422d5bd843909
Joel Besada <joel.besada@gmail.com>
2013-06-08 21:24:55 +0200

    Added a simple websocket server

---
+++ server/app.js
@@ -0,0 +1,62 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , WebSocketServer = require('websocket').server
+  , currentID = 0;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var webServer = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var webSocketServer = new WebSocketServer({
+  httpServer: webServer
+});
+
+webSocketServer.on("request", function(req) {
+  setupConnection(req.accept(null, req.origin));
+});
+
+function setupConnection(connection) {
+  var ID = currentID++;
+  clients[ID] = {
+    connection: connection
+  };
+
+  connection.on("message", function(message) {
+    var client, clientID;
+
+    for (clientID in clients) {
+      client = clients[clientID];
+      if (clientID !== ID) {
+        client.connection.sendUTF(message.utf8Data);
+      }
+    }
+  });
+
+  connection.on("close", function(reasonCode, description) {
+    delete clients[ID];
+  });
+}
